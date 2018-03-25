510f731200a783a4c26c2961d442fa0e60180054
Leo <479711970@qq.com>
2018-03-25 23:04:48 +0800

    preview

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-player/dist/',
+    assetsPublicPath: '/Vue-player/dist/',
 
     /**
      * Source Maps
