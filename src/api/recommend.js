50f14464a647a6d69ef35fb17e7ca8629d700440
Leo <479711970@qq.com>
2018-03-26 22:37:08 +0800

    change port

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -12,7 +12,7 @@ export function getRecommend(){
       return jsonp(url, data, options)
 }
 export function getDiscList(){
-const url = debug ? '/api/getDiscList' : 'http://chenleo.top/api/getDiscList'
+const url = debug ? '/api/getDiscList' : '/music/api/getDiscList'
     const data = Object.assign({}, commonParams, {
         platform: 'yqq',
         hostUin: 0,
@@ -32,7 +32,7 @@ const url = debug ? '/api/getDiscList' : 'http://chenleo.top/api/getDiscList'
 }
 
 export function getSongList(disstid) {
-  const url = debug ? '/api/getCdInfo' : 'http://chenleo.top/api/getCdInfo'
+  const url = debug ? '/api/getCdInfo' : '/music/api/getCdInfo'
   const data = Object.assign({}, commonParams, {
     disstid,
     type: 1,
--- src/api/song.js
+++ src/api/song.js
@@ -7,7 +7,7 @@ import { ERR_OK } from 'api/config'
 const debug = process.env.NODE_ENV !== 'production'
 
 export function getLyric(mid) {
-  const url = debug ? '/api/lyric' : 'http://chenleo.top/api/lyric'
+  const url = debug ? '/api/lyric' : '/music/api/lyric'
 
   const data = Object.assign({}, commonParams, {
     songmid: mid,
@@ -27,7 +27,7 @@ export function getLyric(mid) {
 }
 
 export function getSongsUrl(songs) {
-  const url = debug ? '/api/getPurlUrl' : 'http://chenleo.top/api/getPurlUrl'
+  const url = debug ? '/api/getPurlUrl' : '/music/api/getPurlUrl'
 
   let mids = []
   let types = []
